b14839b0651d49f7cbec572517f19308fb6cfeac
Youkun Wu <ywu338@ti-05.cs.ucr.edu>
2020-06-08 20:09:18 -0700

    work

--- Hashtable.cpp
+++ Hashtable.cpp
@@ -3,62 +3,113 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "Hashtable.h"
-
+#include <math.h>
+#include <unordered_map>
 
 using namespace std;
 
 
 int main(int argc, char** argv) {
 	double loadfactor = 0.5;
-	int sizeoftable = 2000;
+	int sizeoftable = 4096 * 4 * 4 * 4 * 4 * 4 * 4;
 	struct pairs *array0;
 	struct pairs *array1;
+	struct pairs *array2;
 	array0 = new pairs[sizeoftable];
 	array1 = new pairs[sizeoftable];
+	array2 = new pairs[sizeoftable];
 	struct Hashtable *map1 = new Hashtable(sizeoftable,loadfactor,array0);
         struct Hashtable *map2 = new Hashtable(sizeoftable,loadfactor,array1);
-
-	
-	int sizeofarray = 1024;
-	int range = 2000;
-	int threshold = 4000;
-	/*		
-	struct pairs *arraya1 = new pairs[sizeofarray];
-	struct pairs *arraya2 = new pairs[sizeofarray];
-	
-	generatePairs(arraya1, arraya2, sizeofarray);
-	cout<<"fisrt insertion"<<endl;
-	insert(arraya1,0,sizeofarray,map1,1,sizeofarray,100);
-	cout<<"second insertion"<<endl;
-	insert(arraya2,0,sizeofarray,map2,1,sizeofarray,100);
-
-	if(compareTwoArray(map1->hashtable,map2->hashtable,map1->capacity)) {
-                cout<<"Determinism"<<endl;
-
+	struct Hashtable *map3 = new Hashtable(sizeoftable,loadfactor,array2);
+	unordered_map<int, int> umap; 
+ 	double t0, t1,t2,t3;	
+	int sizeofarray = 4096 * 4 * 4 * 4 * 4;
+	int threshold = 256;
+	if (argc < 2) {
+                cout << "Usage: ./a.out [num_elements] [threshold=1]" << endl;
+                return 0;
         }
-	*/
-	
-	for(int i = 1; i < 2; i++) {
-		cout<<"The "<< i <<" iteration"<<endl;
+        sizeofarray = pow(4,atoi(argv[1])) * 4096 ;
+        if (argc == 3) {
+                threshold = atoi(argv[2]);}
+
+	cout<<"size of array is "<<sizeofarray<<endl;
+
+	int range = sizeofarray * 2;
+		
 		struct pairs *arraya1 = new pairs[sizeofarray];
 		struct pairs *arraya2 = new pairs[sizeofarray];
 
 		generatePairs(arraya1,arraya2,sizeofarray,range);
-				
-		cout<<"Collision in first Map"<<endl;
-
-		insert(arraya1,0,sizeofarray,map1,1,sizeofarray,256,range);
 		
-		cout<<"Collision in second Map"<<endl;
-		insert(arraya2,0,sizeofarray,map2,1,sizeofarray,256,range);
+		t2 = get_sec();
+                for(int j = 0; j < sizeofarray; j++) {
+                        insertSingle(arraya1,arraya1[j],map3,sizeofarray,range);
 
 
+                }
+                 t3 = get_sec();
+                 //cout<<"Operation time for inserting single takes time"<< t3 - t2<<endl; 
+
+		
+		//cout<<"Collision in first Map"<<endl;
+		t0 = get_sec();
+
+		insert(arraya1,0,sizeofarray,map1,1,sizeofarray,threshold,range);
+		t1 = get_sec();
+
+		cout<<"Operation time for threshold with "<<threshold<<" takes time"<< t1 - t0<<endl;
+		//cout<<endl;
+		//cout<<endl;	
+		//cout<<"Collision in second Map"<<endl;
+		t2 = get_sec();
+		insert(arraya2,0,sizeofarray,map2,1,sizeofarray,threshold,range);	
+		t3 = get_sec();
+
+		cout<<"Operation time for threshold with "<<threshold<<" takes time"<< t3 - t2<<endl;
+		
+		 /*t2 = get_sec();		
+		for(int j = 0; j < sizeofarray; j++) {
+			insertSingle(arraya1,arraya1[j],map3,sizeofarray,range);
+
+
+		}
+		 t3 = get_sec();
+		 cout<<"Operation time for inserting single takes time"<< t3 - t2<<endl; */
 		if(compareTwoArray(map1->hashtable,map2->hashtable,map1->capacity,range)) {
-                cout<<"Determinism"<<endl;
+                	cout<<"Determinism"<<endl;
 
         	}
-	}
+		t2 = get_sec();	
+		cilk_for(int j = 0; j < sizeofarray; j++) {
+	
+			if(!find(arraya1[j],map1,arraya1,sizeofarray)) {
 
+			cout<<"Could not find, fail"<<endl;
+		        
+			}
+		}
+		t3 = get_sec();
+		cout<<"Find Operation time for takes time"<< t3 - t2<<endl;
+		
+	t0 = get_sec();	
+	for(int i = 0; i < sizeofarray; i++) {
+		umap.insert(make_pair(arraya1[i].key,arraya1[i].value));
+	}
+	t1 = get_sec();
+
+	t2 = get_sec();
+	unordered_map<int,int>::const_iterator got;
+	for(int i = 0; i < sizeofarray; i++) {
+		got = umap.find(arraya1[i].key);
+		if(got == umap.end()) {
+			cout<<"Counld not find"<<endl;
+		}
+	}
+	t3 = get_sec();
+
+	cout<<"Sequential find time for threshold with takes time"<< t3 - t2<<endl;
+	cout<<"Sequential add for sequential takes time"<< t1 - t0<<endl;	
 }
 	/*
 	struct pairs *arraya1 = new pairs[sizeofarray];
--- Hashtable.h
+++ Hashtable.h
@@ -5,14 +5,17 @@
 #include <cilk/cilk.h>
 #include <cilk/cilk_api.h>
 #include <atomic>
+#include <set>
 #include <bits/stdc++.h>
+#include "utils.h"
 using namespace std;
-
+using namespace utils;
 
 
 struct pairs{
- 	int key;
-	int value;
+  	unsigned int key;
+	unsigned int value;
+	unsigned int index;
 	/*pairs(int key,int value) {
 		this->key = key;
 		this->value = value;
@@ -40,7 +43,7 @@ inline unsigned int hashI(unsigned int a)
    a = (a^0xc761c23c) ^ (a>>19);
    return a;
 }
-inline bool SCAS(int *ptr, int oldv, int newv);
+//inline bool SCAS(int *ptr, int oldv, int newv);
 int insert(pairs *array,int startpoint, int sizeofarray,Hashtable *map,int resizingflag,int totalsize,int thresholds,int range);
 int compare_and_swap(Hashtable* a,int index, pairs b, pairs c);
 
@@ -48,56 +51,68 @@ void generatePairs(pairs *array1,pairs *array2, int amount,int range) {
 
 	srand (time(NULL));
 	int index;
+	int i = 0;
 	pairs pair1;
 	pairs pair2;
-	for(int i; i < amount; i++) {
-		pair1.key = rand() % range; 
+	
+	set<int> myset;
+	set<int>::iterator it;
+	while(i < amount ) {
+	
+		pair1.key = rand() % range + 1;
 		pair2.key = pair1.key;
-
-		pair1.value = rand() % range;
+		pair1.value = rand() % range + 1;
 		pair2.value = pair1.value;
-
-		array1[i] = pair1;
-		array2[i] = pair2;
+		pair1.index = i;
+		pair2.index = i;
+		it = myset.find(pair1.key);
+	
+		if(it == myset.end() ) {
+			myset.insert(pair1.key);			
+			array1[i] = pair1;
+			array2[i] = pair2;
+			i++;
+		} else {
+			
+			continue;
+		}
 	}
 
 }
-int compareTwoArray(pairs *array1, pairs *array2,int size,int range) {
-	int flag = 1;
+int compareTwoArray(pairs *array1, pairs *array2,int size,int range) { 
 	for(int i = 0; i < size; i++) {
-		if(array2[i].key <= range && array1[i].key <= range &&  array1[i].key != array2[i].key) {
-			cout<<i<<"    "<<array1[i].key<<"   "<<array2[i].key<<endl;
-			flag = 0;
+		if(array1[i].key != array2[i].key) {
+			return 0;
 		}
-
 	}
-	return flag;
+	return 1;
 
 }
 
 
 int resizing(Hashtable *map,int sizeofarray,int totalsize,int threshold,int resizingtime,int range){
-	//cout<<"before resizing"<<endl;	
+	cout<<"before resizing"<<endl;	
 	if(resizingtime == 1) {
 	int oldcapacity = map->capacity;
 	 int newcapacity = (oldcapacity +sizeofarray)/(map->loadfactor-0.1);
         struct pairs *newarray = new pairs[newcapacity]; 
         double loadfactor = map->loadfactor;
 
-      	int count = 0;
-        pairs *oldarray = new pairs[oldcapacity];
-	for(int i; i < oldcapacity; i++) {
+  	struct pairs *oldarray = new pairs[totalsize]; 
+       	int count = 0; 
+	for(int i = 0; i < oldcapacity; i++) {
 		if(map->hashtable[i].key != 0 && map->hashtable[i].value != 0) {	
 			oldarray[count].key  = map->hashtable[i].key;
 			oldarray[count].value = map->hashtable[i].value;
 			count++;
+		
 		}
 
 	}
 
-        map->hashtable =  newarray;
-	map->capacity = newcapacity;
-        insert(oldarray,0,count,map,0,count,threshold,range);
+        	map->hashtable =  newarray;
+		map->capacity = newcapacity;
+        	insert(oldarray,0,count,map,0,count,threshold,range);
 	} else {
 		 int oldcapacity = map->capacity;
          int newcapacity = (oldcapacity +sizeofarray)/(map->loadfactor-0.1);
@@ -116,119 +131,90 @@ int resizing(Hashtable *map,int sizeofarray,int totalsize,int threshold,int resi
 }
 
 int linearProbing(pairs c, Hashtable *map, int index) {
-//	cout<<"linear probing1"<<endl;
 	int index1 = index;
 	while(map->hashtable[index1].key != 0 && map->hashtable[index1].value != 0) {
 		index1 = (index1 + 5 * map->probing) % map->capacity;		
 		map->probing += 1;
 	}
 	struct pairs v = map->hashtable[index1];
-	int* ptr = &(map->hashtable[index1].key);
-	if(SCAS(ptr,v.key,c.key)) {
-		cout<<"linear compare  "<<v.key<<"     "<<c.key<<endl;
-		//cout<<"linear probing successful"<<endl;
-	} else {
-
-		cout<<"linear probing fail"<<endl;
+	unsigned int* ptr = &(map->hashtable[index1].key);
+	unsigned int* ptr1 = &(map->hashtable[index1].value);
+	if(CAS(ptr,v.key,c.key) && CAS(ptr1,v.value,c.value)) {
+		
 	}
 	return 0;
 
 }
-inline bool SCAS(int *ptr, int oldv, int newv) {
-  unsigned char ret;
-  /* Note that sete sets a 'byte' not the word */
-  __asm__ __volatile__ (
-                "  lock\n"
-                "  cmpxchgl %2,%1\n"
-                "  sete %0\n"
-                : "=q" (ret), "=m" (*ptr)
-                : "r" (newv), "m" (*ptr), "a" (oldv)
-                : "memory");
-  return ret;
-}
 
-
-int compare_and_swap(Hashtable* a,int index, pairs b, pairs c) {
-
-	if(a->hashtable[index].key == b.key && a->hashtable[index].value == b.value) {
-
-		a->hashtable[index].key = c.key;
-		a->hashtable[index].value = c.value;
-	
-		return 1;
-	
-	} else {
-
-		a->hashtable[index].key= b.key;
-		a->hashtable[index].value = b.value;
-		return 0;
-
-	}
-
-
-
-}
-
+/*
 int prioritycheck(pairs* array, int totalsize, pairs c){
-	for(int j = 0; j < totalsize; j++) {
-             	if(c.key == array[j].key) {                                       
-                       return j;
-               	}
-        }
-	return (totalsize + 2);
+	int flag = totalsize + 2;
+	
+		for(int j = 0; j < totalsize; j++) {
+             		if(c.key == array[j].key && c.value == array[j].value) {                                       
+                       		return j;
+               		}
+        	}
+		for(int j = 0; j < totalsize; j++) {
+			if(c.key == array[j].key) {
+                                return j;
+                        }
+		}
+	cout<<"Priority check not found C "<<c.key<<"   "<<c.value<<endl;
+	return flag;
+}*/
+double get_sec()
+{
+	struct timeval time;
+	gettimeofday(&time, NULL);
+	return (time.tv_sec + 1e-6 * time.tv_usec);
 }
-
-int insertSingle(pairs *array, int i, Hashtable *map,int totalsize,int range) {
-	int toHash = array[i].key;	
-	int index = (int)(hashI(toHash) % map->capacity);
-                    struct pairs v;
-			v.key =  array[i].key;
-			v.value = array[i].value;
-                    while(v.key != 0 && v.key < range) {
+int insertSingle(pairs *array, pairs p, Hashtable *map,int totalsize,int range) {	
+	int toHash1 = p.key;
+	int index = (int)(hashI(toHash1) % map->capacity);
+        struct pairs v = p;
+                    while(v.key != 0 && v.value != 0) {
 			int index2 = -1;
-                        struct pairs c;
-			c.key = map->hashtable[index].key;
-			c.value = map->hashtable[index].value;
+			int index3 = -1;
+                        struct pairs c = map->hashtable[index];
 			if(c.key == v.key) {
                                 return 1;
                         } else { 
-				if(c.key != 0) {
-					index2 = prioritycheck(array,totalsize,c);
-					if( index2 <= i) {
+				if(c.key != 0 && c.key < range) {
+				
+					/*index2 = prioritycheck(array,totalsize,c);
+					index3 = prioritycheck(array,totalsize,v);*/
+					index2 = c.index;
+					index3 = v.index;
+					if(index2 < index3) {
 							index += 1;
 							continue;
-                                         } else if (index2 > i && index2 != totalsize+2) {
-						int* ptr = &(map->hashtable[i].key); 
-						if(SCAS(ptr,c.key,v.key)) {
-								cout<<"compare priority  "<<c.key<<"    "<<v.key<<endl;
-                                                                v.key = c.key;
-                                                                v.value = c.value;
+                                         } else if (index2 > index3 && index2 != totalsize+2) {
+				
+						unsigned int* ptr = &(map->hashtable[index].key); 
+						unsigned int* ptr1 = &(map->hashtable[index].value);
+						unsigned int* ptr2 = &(map->hashtable[index].index);
+						if(CAS(ptr,c.key,v.key)&& CAS(ptr1,c.value,v.value)&&CAS(ptr2,c.index,v.index)) {
+						                v = c;
                                                                 index += 1;
-                                                } else {
-
-							cout<<"priority compare fail"<<endl;
-						}
-					 }        
+								if(v.value == 0) {
+									return 1;
+								}
+                                                }
+					 } else if(index2 == totalsize +2 && c.key < range) {
+                                                if(linearProbing(v, map,index)) {
+                                                        return 1;
+                                                }	
+					 }     
                            	} else  {
-					int* ptr = &(map->hashtable[i].key);
-
-					if(SCAS(ptr,c.key,v.key)) {                  
+					unsigned int* ptr = &(map->hashtable[index].key);
+					unsigned int* ptr1 = &(map->hashtable[index].value);
+					unsigned int* ptr2 = &(map->hashtable[index].index);
+					if(CAS(ptr1,c.value,v.value) && CAS(ptr,c.key,v.key)&&CAS(ptr2,c.index,v.index)) {                  
                                                                 return 1;
 
-                                         } else {
-						cout<<"zero compare fail  "<<c.key<<"      "<<v.key<<endl;
-					}	
+                                         }
 				}
-
-				if(index2 == totalsize + 2) {
-					if(v.key < range && c.key <range) {
-                                        	if(linearProbing(v, map,index)) {
-                                                	return 1;
-						}
-					} else {
-						return 0;	
-					}
-                                }
 				continue;	
                         }
 
@@ -238,31 +224,23 @@ int insertSingle(pairs *array, int i, Hashtable *map,int totalsize,int range) {
 }
 
 int insert(pairs *array,int startpoint, int sizeofarray,Hashtable *map,int resizingflag,int totalsize,int thresholds,int range){
-	int threshold = thresholds;
-
 	if(resizingflag == 1 && ((map->size + sizeofarray) > (map->loadfactor * map->capacity))) {
-
 		if(map->size != 0) {
-
-			resizing(map,sizeofarray,totalsize,threshold,1,range);
+			resizing(map,sizeofarray,totalsize,thresholds,1,range);
 		} else {
-			resizing(map,sizeofarray,totalsize,threshold,0,range);
-		}
+			resizing(map,sizeofarray,totalsize,thresholds,0,range);
+		}		
 	}
-
-
-	if(sizeofarray <= threshold) {
-		int index;
-		for(int i = startpoint; i < (startpoint + sizeofarray); i++) {
-			if(array[i].key < range) {
-				insertSingle(array,i,map,totalsize,range);
-			}
+	if(sizeofarray <= thresholds) {
+		for(int i = startpoint; i < (startpoint + sizeofarray); i++) {			
+				insertSingle(array,array[i],map,totalsize,range);
+			
 		}
 	} else {
-		cilk_spawn insert(array,startpoint,sizeofarray/4, map,0,totalsize,threshold,range);
-		cilk_spawn insert(array,startpoint+sizeofarray/4,sizeofarray/4, map,0,totalsize,threshold,range);
-		cilk_spawn insert(array,startpoint+sizeofarray/2,sizeofarray/4, map,0,totalsize,threshold,range);
-			   insert(array,startpoint+(sizeofarray*3/4),sizeofarray/4, map,0,totalsize,threshold,range);
+		cilk_spawn insert(array,startpoint,sizeofarray/4, map,0,totalsize,thresholds,range);
+		cilk_spawn insert(array,startpoint+sizeofarray/4,sizeofarray/4, map,0,totalsize,thresholds,range);
+		cilk_spawn insert(array,startpoint+sizeofarray/2,sizeofarray/4, map,0,totalsize,thresholds,range);
+			   insert(array,startpoint+(sizeofarray*3/4),sizeofarray/4, map,0,totalsize,thresholds,range);
 		cilk_sync;
 		if(resizingflag == 1) {
 			map->size = map->size + sizeofarray;
@@ -270,3 +248,39 @@ int insert(pairs *array,int startpoint, int sizeofarray,Hashtable *map,int resiz
 	}
 }
 
+bool find(pairs p, Hashtable *map,pairs *array, int sizeofarray) {
+	int toHash1 = p.key;
+        
+        int index = (int)(hashI(toHash1) % map->capacity);
+
+        int index1 = -1;
+	int index2 = -1;
+	
+	struct pairs v = p;
+	index1 = v.index;
+	//index1 = prioritycheck(array,sizeofarray,v);
+	int count = 0;
+	
+	while(map->hashtable[index].key != 0) {
+		count += 1;
+		struct pairs c = map->hashtable[index];
+        	//index2 = prioritycheck(array,sizeofarray,c);
+        	index2 = c.index;
+		if(index1 == sizeofarray + 2) {
+			
+			cout<<"not found"<<endl;
+			return false;
+		} else if(index1 == index2) {
+			if((c.key == v.key && c.value == v.value)) {
+				return true;
+			} 
+		} else if(index1 >  index2){
+			index += 1;
+		} else {
+			return false;
+
+		}
+	}
+
+
+}
